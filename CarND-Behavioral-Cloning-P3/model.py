56f987d3913b5744b0e3f94c3de82d0e9f447557
Philippe Weingertner <philippe.weingertner@gmail.com>
2017-03-30 20:46:10 +0200

    CarND-Behavioral-Cloning-P3 sort of baseline (with minimalistic training data)

--- CarND-Behavioral-Cloning-P3/model.py
+++ CarND-Behavioral-Cloning-P3/model.py
@@ -9,54 +9,88 @@ Created on Wed Mar 29 19:42:48 2017
 import csv
 import cv2
 import numpy as np
+import sklearn
+
 
 lines = []
 with open('./data/driving_log.csv') as csvfile:
+    next(csvfile, None)
     reader = csv.reader(csvfile)
     for line in reader:
         lines.append(line)
 
-del lines[0]
-        
 images = []
 measurements = []
 
 for line in lines:
-    #print(line)
-    source_path = line[0]
-    filename = source_path.split('/')[-1]
-    local_path = "./data/IMG/" + filename
-    #print(local_path)
-    image = cv2.imread(local_path)
-    images.append(image)
-    measurement = float(line[3])
-    #print(measurement)
-    measurements.append(measurement)
+    for i in range(3):
+        # Load images from center, left and right cameras
+        source_path = line[i]
+        tokens = source_path.split('/')
+        filename = tokens[-1]
+        local_path = "./data/IMG/" + filename
+        image = cv2.imread(local_path)
+        images.append(image)
 
-X_train = np.array(images)
-y_train = np.array(measurements)
-print(X_train.shape)
-print(y_train.shape)
+    # Introduce steering correction
+    correction = 0.2
+    measurement = float(line[3])
+    # Steering adjustment for center images
+    measurements.append(measurement)
+    # Add correction for steering for left images
+    measurements.append(measurement+correction)
+    # Minus correction for steering for right images
+    measurements.append(measurement-correction)
+
+augmented_images = []
+augmented_measurements = []
+
+# Augmented data set by adding 'flipped' images 
+# so model can learn from reversed images, 
+#as well as random brightness 
+#(with thanks to Vivek Yadav at http://bit.ly/2kOk6MU for the latter)
+for image, measurement in zip(images, measurements):
+    augmented_images.append(image)
+    augmented_measurements.append(measurement)
+
+    #brightened_image = cv2.cvtColor(image,cv2.COLOR_RGB2HSV)
+    #random_bright = .25+np.random.uniform()
+    #brightened_image[:,:,2] = brightened_image[:,:,2]*random_bright
+    #brightened_image = cv2.cvtColor(brightened_image,cv2.COLOR_HSV2RGB)
+    #flipped_image = cv2.flip(brightened_image, 1)
+
+    flipped_image = cv2.flip(image, 1)
+    flipped_measurement = measurement * -1.0
+    augmented_images.append(flipped_image)
+    augmented_measurements.append(flipped_measurement)
+
+# Pull the image and steering measurements 
+# into NumPy arrays we can use in the model
+X_train = np.array(augmented_images)
+y_train = np.array(augmented_measurements)
 
 import keras
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Activation, Convolution2D, MaxPooling2D, Dropout, Lambda
+from keras.layers import Flatten, Dense, Lambda
+from keras.layers.convolutional import Convolution2D, Cropping2D
+from keras.layers.pooling import MaxPooling2D
 
+# Model based on Nvidia's end-to-end architecture
 model = Sequential()
-# Normalizing lambda layer
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
-# First convolutional layer
-model.add(Convolution2D(6,5,5,activation='relu'))
-model.add(MaxPooling2D())
-# Second convolutional layer
-model.add(Convolution2D(6,5,5,activation='relu'))
-model.add(MaxPooling2D())
+model.add(Cropping2D(cropping=((70,25),(1,1))))
+model.add(Convolution2D(24,5,5, subsample=(2,2),activation='relu'))
+model.add(Convolution2D(36,5,5,subsample=(2,2),activation='relu'))
+model.add(Convolution2D(48,5,5,subsample=(2,2),activation='relu'))
+model.add(Convolution2D(64,3,3,activation='relu'))
+model.add(Convolution2D(64,3,3,activation='relu'))
 model.add(Flatten())
-# Fully-connected layers
-model.add(Dense(120))
-model.add(Dense(84))
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
 model.add(Dense(1))
 
-model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7, verbose=2)
+model.compile(optimizer='adam', loss='mse')
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
+
 model.save('model.h5')
